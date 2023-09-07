a5d12660b98f4969a30b6345bd189901c63d0038
josephcollins-toast <joseph.collins@toasttab.com>
2023-09-07 10:27:12 -0500

    minor css updates

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-//in git and on local comp - adjust title of old file to be OLD - do a test push to verify they are still connected
-
-//in git and on local comp - adjust title of this file to be just 'pick_em' - do a test push to verify they are still connected
-
 const express = require("express");
 const app = express();
 const syncAndSeed = require("./server/script/seed");
